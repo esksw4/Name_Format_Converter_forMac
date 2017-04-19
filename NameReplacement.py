2a6d8b064fb323397129afb7925b410e45f3f616
esksw4 <e.kim4.backup@gmail.com>
2017-04-19 19:35:06 -0400

    need to do GUI part
    ads

---
+++ NameReplacement.py
@@ -0,0 +1,16 @@
+# If Filename does not start with 'fromWhatName',
+# then change the name to 'toWhatNameReplace'.
+# fileDirect: should contain exact directory for that folder
+
+import glob, os
+
+fileDirect = "/Users/eun-solkim/BBox-Label-Tool-master/"
+fromWhatName = "DonotEnter"
+toWhatNameReplace = ""
+
+i = 0
+for filename in os.listdir(fileDirect):
+	if filename != ".DS_Store":
+		if !filename.startswith(fromWhatName):
+			os.rename(fileDirect + "%s" %(filename), fileDirect + toWhatNameReplace + "%d.JPEG" % (i))
+	i += 1
